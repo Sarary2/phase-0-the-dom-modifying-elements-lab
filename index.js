403041660175e35bf06929f8d42ba89676eb26a7
Sara Youssef <sarary2000@gmail.com>
2022-10-20 19:02:17 +0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+const element = document.getElementById('main');
+
+element.remove();
+
+const newHeader = document.createElement("h1")
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML ="<p>Sara is the champion</p>"
+
+//const newHeader=document.getElementById("victory").querySelectorAll("h1");
+//newHeader;
+//document.body.append(h1);
